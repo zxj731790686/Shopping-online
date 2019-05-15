739e16623ba4a8dadaeff6a2a6aede1bb09a3d5a
moon <45783776+mrcxrzs1314@users.noreply.github.com>
2019-05-15 20:24:14 +0800

    Add files via upload

---
+++ shopping_online/static/js/base.js
@@ -0,0 +1,32 @@
+  $(function () {
+        var Accordion = function (el, multiple) {
+            this.el = el || {};
+            this.multiple = multiple || false;
+
+            // Variables privadas
+            var links = this.el.find('.link');
+            // Evento
+            links.on('click', {
+                el: this.el,
+                multiple: this.multiple
+            }, this.down)
+        };
+
+        Accordion.prototype.down = function (e) {
+            var $el = e.data.el;
+            $this = $(this);
+            $next = $this.next();
+
+            $next.slideToggle();
+            $this.parent().toggleClass('open');
+
+            if (!e.data.multiple) {
+                $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+            }
+        };
+
+        var accordion = new Accordion($('#accordion'), false);
+        $('.submenu li').click(function () {
+            $(this).addClass('current').siblings('li').removeClass('current');
+        });
+    });
